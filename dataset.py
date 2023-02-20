9049c26b93aeb56e1e0ef6ab4bb55c841ca9727b
Sanho LEE <stlsh0516@gmail.com>
2023-02-21 07:39:27 +0900

    Add Transform classes. ToTensor, Normalization, RandomFlip.

--- dataset.py
+++ dataset.py
@@ -23,11 +23,11 @@ class Dataset(torch.utils.data.Dataset):
         self.lst_label = lst_label
         self.lst_input = lst_input
 
-##
+##  Data length, method ocf // len()
     def __len__(self):
         return len(self.lst_label)
 
-##
+##  Get a specific index data, method of // instanceName(index)
     def __getitem__(self, index):
         # 인덱스 선택으로 해당 데이터를 가져올 수 있는 built-in method.
 
@@ -55,9 +55,60 @@ class Dataset(torch.utils.data.Dataset):
         return data
 
 
-## Trnasform class
-# class ToTensor(object):
+## Transform classes, It will be used when creating transform combination, likes 'transforms.Compose([...])'
+class ToTensor(object):
+    '''
+    chagne data type : numpy to tensor
+    '''
+    def __call__(self, data):
+        label, input = data['label'], data['input']
 
+        # numpy to tensor, change and set to tensor-manner in terms of its index position.
+        # (Todo..)numpy index for img : ( , , )
+        # (Todo..)tensor index for img : ( , , )
+        label = label.transpose((2,0,1)).astype(np.float32)
+        input = input.transpose((2,0,1)).astype(np.float32)
 
+        # map to return data
+        data = {'label': torch.from_numpy(label), 'input':torch.from_numpy(input) }
 
+        return data
 
+class Normalization(object):
+    '''
+    Nomarlizing pixel values
+    '''
+
+    def __init__(self, mean=0.5, std=0.5):
+        self.mean = mean
+        self.std = std
+    def __call__(self, data):
+        # Only applying to input data,
+        # (Todo Checking) Probably, since label data is consist of only 0 and 1,
+        #  binary data, don't need to normalization.
+
+        label, input = data['label'], data['input']
+
+        input = (input - self.mean) / self.std
+
+        data = {'label': label, 'input': input}
+        return data
+
+class RandomFlip(object):
+    '''
+    Literally, Flip data Left and Right, Up and Down Randomly
+    '''
+    def __call__(self, data):
+        label, input = data['label'], data['input']
+
+        # 난수 생성에 의한 데이터 조작
+        if np.random.rand() > 0.5:
+            label = np.fliplr(label)
+            input = np.fliplr(label)
+
+        if np.random.rand() > 0.5:
+            label = np.flipud(label)
+            input = np. flipud(input)
+
+        data = {'label': label, 'input': input}
+        return data
